a202596f28b58cb812639f640e5ceae4ca234da2
imparikh <parikh.i.m@gmail.com>
2017-12-04 15:28:15 -0500

    randomize

--- js/catalog.js
+++ js/catalog.js
@@ -10,21 +10,36 @@ $("#semester").change(function() {
   update($('#semester').val());
 });
 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
 function update(semester) {
   var input = $("#catalog").val().toUpperCase();
   var classesToDisplay = "";
-  var showArchvies = false;
-  if ($("#check").is(":checked")) {
-    showArchvies = true;
-  }
   var classes = CLASSES.classes[semester].classes;
+  var classArr = []
   for (var i in classes) {
     var course = classes[i];
     var toTestStr = course.department + course.number;
     if (toTestStr.indexOf(input) == 0 || input === "") {
-      classesToDisplay += createDiv(course);
+      classArr.push(createDiv(course));
     }
   }
+  classArr = shuffle(classArr)
+  classesToDisplay = classArr.join('')
   if (classesToDisplay === "")
     classesToDisplay = "No classes found with that course code.";
   $("#displayedCourses").html(classesToDisplay);
