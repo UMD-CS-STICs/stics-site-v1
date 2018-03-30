5a234e15353259d5818e00e04fa4f9d32ad77d75
David Ng <davidn6597@gmail.com>
2018-03-30 15:38:35 -0400

    removed shuffling on catalog items

--- js/catalog.js
+++ js/catalog.js
@@ -11,20 +11,20 @@ $("#semester").change(function() {
 });
 
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex;
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-  return array;
-}
+// function shuffle(array) {
+//   var currentIndex = array.length, temporaryValue, randomIndex;
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     // Pick a remaining element...
+//     randomIndex = Math.floor(Math.random() * currentIndex);
+//     currentIndex -= 1;
+//     // And swap it with the current element.
+//     temporaryValue = array[currentIndex];
+//     array[currentIndex] = array[randomIndex];
+//     array[randomIndex] = temporaryValue;
+//   }
+//   return array;
+// }
 
 function update(semester) {
   var input = $("#catalog").val().toUpperCase();
@@ -38,7 +38,7 @@ function update(semester) {
       classArr.push(createDiv(course));
     }
   }
-  classArr = shuffle(classArr)
+  classArr.sort();
   classesToDisplay = classArr.join('')
   if (classesToDisplay === "")
     classesToDisplay = "No classes found with that course code.";
